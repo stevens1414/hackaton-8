ea97bca945e286eda83f4704f1e3714856ace3f1
christopher <stevens14141@gmial.com>
2020-12-12 11:12:27 -0500

    ejercicios

--- ejercicios-js/votar.js
+++ ejercicios-js/votar.js
@@ -1,13 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script>
-
 function datos(edad) {
     const edadMayor = 18;
     if ( edad > edadMayor){
@@ -21,6 +11,3 @@ function datos(edad) {
 
    }
    main()
-    </script>
-</body>
-</html>
