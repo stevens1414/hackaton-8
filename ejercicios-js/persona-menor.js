e522b719bc6aaa2e181922ada90bf29a99629b2e
christopher <stevens14141@gmail.com>
2020-12-11 23:21:29 -0500

    ejercicios

---
+++ ejercicios-js/persona-menor.js
@@ -0,0 +1,18 @@
+// obtener persona menor 
+let p1 = {
+    name:calos,
+    edad: 23
+}
+let p2 = {
+    name:juan,
+    edad: 17
+}
+let p3 = {
+    name:marco,
+    edad: 25
+}
+
+if (p1.edad < p2.edad) {
+   return
+}
+
